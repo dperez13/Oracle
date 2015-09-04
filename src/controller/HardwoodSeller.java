ee2706b4b52dc4720184760e27a028ce79b225df
dperez13 <davidpe131@gmail.com>
2015-09-03 22:08:23 -0400

    Created method stubs, modified HardwoodSeller class
    Created method stubs for initializing buyer and order information, implemented some of the
    main method logic. Began modification of readInputFile.

--- src/controller/HardwoodSeller.java
+++ src/controller/HardwoodSeller.java
@@ -3,6 +3,10 @@
  */
 package controller;
 
+import model.WoodItem;
+import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.lang.Exception;
 /**
  * @author Esteban
  *
@@ -13,13 +17,56 @@ public class HardwoodSeller {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Assumption is that args[0] will contain filename for input text. 
+		if(args.length < 1)
+			System.exit(-1);	//Exit if we do not provide a filename. 
+		HardwoodSeller theDealer = new HardwoodSeller(); 
+		//Maintaining parallel arraylists with buyer and seller information.
+		ArrayList<String> buyerStrings = new ArrayList<String>();
+		ArrayList<String> orderStrings = new ArrayList<String>();
+		ArrayList<ArrayList<String> > buyerInformation = new ArrayList<ArrayList<String>>(); //First dimension contains a single buyer, second dimension holds full name, address, date.
+		ArrayList<ArrayList<WoodItem> > orderInformation = new ArrayList<ArrayList<WoodItem>>(); 
+		theDealer.readInputFile(args[0], buyerStrings, orderStrings);
+		for(String buyer: buyerStrings){
+			ArrayList<String> temp = theDealer.initBuyerInfo(buyer);
+			buyerInformation.add(temp);
+		}
+		for(String order: orderStrings){
+			ArrayList<WoodItem> temp = theDealer.initOrderInfo(order);
+			orderInformation.add(temp);
+		}
 	}
-	
-	public void readInputFile(String inputFilePath){
-		
-	}
+	//Modified read array to take lists that will be initialized here. 
+	public void readInputFile(String inputFilePath, ArrayList<String> s1, ArrayList<String> s2){
+		 FileInputStream inFile = null;
+		try{
+         inFile = new FileInputStream(inputFilePath);
+         //TODO: finish up reading the file. 
+		}catch(Exception e){
+			//Keep on chugging
+		}
+		finally {
+		 if (inFile != null) {
+			try { 
+				inFile.close();
+			}catch(Exception e) {
+				//Keep on chugging deux
+			}
+		 }
+		}
+   }
+   
+   public ArrayList<String> initBuyerInfo(String buyerString) {
+	   ArrayList<String> ret = new ArrayList<String>();
+	   //TODO: Parse string into buyer info.
+	   return ret;
+   }
+   public ArrayList<WoodItem> initOrderInfo(String orderString) {
+	   ArrayList<WoodItem> ret = new ArrayList<WoodItem>();
+	   //TODO: Parse string into new wood item. 
+	   return ret;
+   }
 	
 	public Double deliveryTime(){
 		Double deliveryETA = 0.0;
